222ad3e40d3ee23f30346927af632306a86cfeee
Glauco Gonçalves <glaestgon@gmail.com>
2014-07-10 11:43:25 -0300

    New pulp files

--- vncsimulator/MinimizarEnergia.py
+++ vncsimulator/MinimizarEnergia.py
@@ -1,12 +1,12 @@
 """
-The Largest Profit Problem for the PuLP Modeller
+The Minimize Energy Problem for the PuLP Modeller
 
 Authors: Felipe Fernandes, Dr Glauco Goncalves 2014
 """
 from random import randint
 from pulp import *
 
-V=10 #number of virtual machines
+V=5 #number of virtual machines
 M=2 #number of servers
 profit=[randint(20,100) for i in range(0,V)]
 print("Profit:%s"%profit)
@@ -14,6 +14,11 @@ demand=[randint(10,50) for i in range(0,V)]
 print("Demands:%s"%demand)
 capacity=[randint(40,50) for i in range(0,M)]
 print("Capacities:%s"%capacity)
+energycost=[randint(60,100) for i in range(0,M)]
+print("Energy Cost:%s"%energycost)
+
+#demand=[10,30,10,10,20]
+#capacity=[80,30]
 
 vms = [ "vm%s"%i for i in range(1,V+1) ]
 servers = [ "s%s"%i for i in range(1,M+1) ]
@@ -21,28 +26,32 @@ servers = [ "s%s"%i for i in range(1,M+1) ]
 p = dict(zip(vms,profit))
 d = dict(zip(vms,demand))
 C = dict(zip(servers,capacity))
+w = dict(zip(servers,energycost))
 
 # Creates the 'prob' variable to contain the problem data
-prob = LpProblem("Maior Lucro",LpMaximize)
+prob = LpProblem("Minimize Energy",LpMaximize)
 
 # Creates a list of tuples containing all the possible 
 Possible = [(vm,server) for vm in vms for server in servers]
 
 # A dictionary called 'Vars' is created to contain the referenced variables
-vars = LpVariable.dicts("x",(vms,servers),0,1,LpInteger)
+Xvars = LpVariable.dicts("x",(vms,servers),0,1,LpInteger)
+Yvars = LpVariable.dicts("y",servers,0,1,LpInteger)
 
 ##### The objective function is added to 'prob' first
-prob += lpSum([p[vm]*vars[vm][server] for (vm,server) in Possible]), "Sum of profit of vms"
-
+lst1 =[p[vm]*Xvars[vm][server] for (vm,server) in Possible]
+lst2 =[w[server]*(1-Yvars[server]) for server in servers]  
+prob += lpSum(lst1+lst2), "Total Profit"
 
 for server in servers:
-    prob += lpSum([d[vm]*vars[vm][server] for vm in vms])<=C[server], "Sum_of_VM_demands_on_Server_%s"%server
+    prob += lpSum([d[vm]*Xvars[vm][server] for vm in vms])<=Yvars[server]*C[server], "Sum_of_VM_demands_on_Server_%s"%server
 
 for vm in vms:
-    prob += lpSum([vars[vm][server] for server in servers])<=1, "Allocation_Constraint_of_VM_%s"%vm
+    prob += lpSum([Xvars[vm][server] for server in servers])<=1, "Allocation_Constraint_of_VM_%s"%vm
 
+#prob += lpSum([Yvars[server] for server in servers])<=M, "Sanity check"
 # The problem data is written to an .lp file
-prob.writeLP("Maior Lucro.lp")
+prob.writeLP("MinimizarEnergia.lp")
 
 # The problem is solved using PuLP's choice of Solver
 prob.solve()
@@ -55,4 +64,4 @@ for v in prob.variables():
     print v.name, "=", v.varValue
 
 # The optimised objective function value is printed to the screen    
-print "Maior Lucro = ", value(prob.objective)
+print "Total Profit = ", value(prob.objective)
